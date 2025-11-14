9b1e63d04c02e1956737f1035cacb203ac0c8982
edward <edwardlfish@gmail.com>
2025-11-14 14:03:13 +0200

    Integrate Tailwind CSS and enhance UI with updated styles and structure.

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,13 +1,12 @@
-import {store} from "../configureStore/store.js";
-import {useSyncExternalStore} from "react";
+import {useSelector} from "react-redux";
 
 const Balance = () => {
-    const {balance} = useSyncExternalStore(store.subscribe, store.getState);
+    const balance = useSelector(state => state.balance)
 
     return (
-        <div>
-            <h1>Bank</h1>
-            <h2>Balance = {balance}</h2>
+        <div className={'text-center text-uppercase'}>
+            <h1 className={'font-bold text-8xl text-[gold]'}>Iron Bank of Braavos</h1>
+            <h2 className={'text-7xl text-[firebrick]'}>Balance = {balance}</h2>
         </div>
     )
 }
--- src/components/Operation.jsx
+++ src/components/Operation.jsx
@@ -1,19 +1,25 @@
 import {useState} from "react";
 import {deposit, withdraw} from "../actions/accountActions.js";
-import {store} from "../configureStore/store.js";
+import {useDispatch} from "react-redux";
 
 const Operation = () => {
     const [sum, setSum] = useState(0);
+    const dispatch = useDispatch();
 
     return (
-        <div>
-            <button onClick={() => store.dispatch(withdraw(sum))}>Withdraw</button>
+        <div className={'flex justify-center mt-10'}>
+            <button
+                className={'bg-blue-500 hover:bg-blue-600 text-white font-bold rounded-lg py-2 px-4'}
+                onClick={() => dispatch(withdraw(sum))}>Withdraw</button>
             <input
+                className={'text-center border rounded-lg text-lg font-bold mx-2 text-white'}
                 type="number"
                 value={sum}
-                onChange={e => setSum(+e.target.value)}
+                onChange={e => setSum(Math.abs(+e.target.value))}
             />
-            <button onClick={() => store.dispatch(deposit(sum))}>Deposit</button>
+            <button
+                className={'bg-blue-500 hover:bg-blue-600 text-white font-bold rounded-lg py-2 px-4'}
+                onClick={() => dispatch(deposit(sum))}>Deposit</button>
         </div>
     )
 }
