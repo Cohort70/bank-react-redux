c0107167961f109ffcf86831c732697ef30b21eb
edward <edwardlfish@gmail.com>
2025-11-17 12:04:14 +0200

    Add logger enhancer middleware to Redux store for action/state debugging

--- src/configureStore/store.js
+++ src/configureStore/store.js
@@ -1,9 +1,10 @@
-import {createStore} from "redux";
+import {applyMiddleware, createStore} from "redux";
 import {accountReducer} from "../reducer/accountReducer.js";
+import {loggerEnhancer} from "../enhancers/loggerEnhancer.js";
 
 const initialState = {
     balance: 0,
     quote: 'Winter is coming...'
 }
 
-export const store = createStore(accountReducer, initialState);
+export const store = createStore(accountReducer, initialState, applyMiddleware(loggerEnhancer));
