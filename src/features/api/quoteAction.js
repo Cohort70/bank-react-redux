8b429c86193adaf9d733c33e900daa40b593274c
edward <edwardlfish@gmail.com>
2025-11-18 12:07:38 +0200

    Refactor quote fetching logic to use `createAsyncThunk`. Remove custom reducer and integrate `extraReducers` in `quoteSlice` for improved state handling.

--- src/features/api/quoteAction.js
+++ src/features/api/quoteAction.js
@@ -1,11 +1,10 @@
-import {putQuote} from "../quote/quoteSlice.js";
+import {createAsyncThunk} from "@reduxjs/toolkit";
 
-export const fetchQuote = () => {
-    return dispatch => {
-        dispatch(putQuote('Loading...'));
-        fetch('https://api.gameofthronesquotes.xyz/v1/random')
-            .then(response => response.json())
-            .then(data => dispatch(putQuote(data.sentence)))
-            .catch(() => dispatch(putQuote('Error fetching quote')))
+export const fetchQuote = createAsyncThunk(
+    'quote/fetch',
+    async () => {
+        const response = await fetch('https://api.gameofthronesquotes.xyz/v1/random');
+        const data = await response.json();
+        return data.sentence;
     }
-}
+)
--- src/features/quote/quoteSlice.js
+++ src/features/quote/quoteSlice.js
@@ -1,13 +1,16 @@
 import {createSlice} from "@reduxjs/toolkit";
+import {fetchQuote} from "../api/quoteAction.js";
 
 const quoteSlice = createSlice({
     name: 'quote',
     initialState: 'Winter is coming...',
-    reducers: {
-        putQuote: (state, action) => action.payload
+    reducers: {},
+    extraReducers: builder => {
+        builder
+            .addCase(fetchQuote.pending, () => 'Loading...' )
+            .addCase(fetchQuote.rejected, () => 'Error loading quote' )
+            .addCase(fetchQuote.fulfilled, (state, action) => action.payload)
     }
 })
 
-// type: quote/putQuote
-export const {putQuote} = quoteSlice.actions;
 export default quoteSlice.reducer;
