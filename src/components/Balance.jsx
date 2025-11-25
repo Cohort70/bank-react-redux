f1f6f659c214cffd0207761504260ef6076d9c81
edward <edwardlfish@gmail.com>
2025-11-25 12:14:27 +0200

    Refactor `Balance` component to use `useMemo` for `info` and integrate constants for random name selection. Update `Bank` component to handle `info` as an object.

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,15 +1,15 @@
 import {useSelector} from "react-redux";
 import Bank from "./Bank.jsx";
-import {useRef} from "react";
+import {useMemo} from "react";
+import {getRundomIndex, names} from "../utils/constants.js";
 
 const Balance = () => {
     const balance = useSelector(state => state.balance)
     const quote = useSelector(state => state.quote)
-    // const info = {name: 'Braavos'}
-    const info = useRef({name: 'Braavos'})
+    const index = getRundomIndex(names.length)
+    const info = useMemo(() => ({name: names[index]}), [index])
 
-
-    console.log('Render Balance')
+    console.log(`Render Balance index: ${index}`)
 
     return (
         <div className={'text-center text-uppercase'}>
---
+++ src/utils/constants.js
@@ -0,0 +1,2 @@
+export const names = ['Braavos', 'Reverlands', 'Westerlands', 'Crownlands'];
+export const getRundomIndex = bound => Math.trunc(Math.random() * bound);
